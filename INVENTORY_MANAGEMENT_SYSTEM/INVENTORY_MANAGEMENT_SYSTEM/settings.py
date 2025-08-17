743ef7e6ffbc1bea9d1ea28977c05806cb44590a
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-08-17 07:03:25 +0100

    created user app, for user  registration and auth

--- INVENTORY_MANAGEMENT_SYSTEM/INVENTORY_MANAGEMENT_SYSTEM/settings.py
+++ INVENTORY_MANAGEMENT_SYSTEM/INVENTORY_MANAGEMENT_SYSTEM/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-       # Third-party apps
+    # Third-party apps
     'rest_framework',
 
     # My apps
-    
+    'user'
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
