c553514c5b15a282fd721aac7cb18ff5ef2cba50
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-08-25 14:27:42 +0100

    added sales and purchase ap models in django admin to test funtionality

--- INVENTORY_MANAGEMENT_SYSTEM/sales/admin.py
+++ INVENTORY_MANAGEMENT_SYSTEM/sales/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import SalesItem, SalesOrder
 # Register your models here.
+
+@admin.register(SalesItem)
+class SalesItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'price')  
+
+@admin.register(SalesOrder)
+class SalesOrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'date', 'total_amount')  
