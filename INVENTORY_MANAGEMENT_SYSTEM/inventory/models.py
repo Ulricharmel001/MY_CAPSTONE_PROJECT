267632283e13ef64bfef2906d1c3690a5df76115
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-08-25 14:41:26 +0100

    check and made adjustment on inventory models after testing

--- INVENTORY_MANAGEMENT_SYSTEM/inventory/models.py
+++ INVENTORY_MANAGEMENT_SYSTEM/inventory/models.py
@@ -52,7 +52,7 @@ class Supplier(models.Model):
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True, null=True, unique=True)
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.IntegerField(max_length=12, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
